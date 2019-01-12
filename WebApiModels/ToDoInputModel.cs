9219803451c837e1e34157771c2bdb2007d51d29
Eva <eva_only@abv.bg>
2019-01-13 00:20:08 +0200

    asp net core consuming web api demo - viewModels

---
+++ WebApiModels/ToDoInputModel.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApiModels
+{
+    public class ToDoInputModel
+    {
+        public string JsonObj { get; set; }
+
+        public int Id { get; set; }
+
+        [JsonProperty(PropertyName = "Name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "IsComplete")]
+        [Display(Name = "Completed")]
+        public bool IsComplete { get; set; }
+
+        public IEnumerable<ToDoViewModel> Items { get; set; } = new HashSet<ToDoViewModel>();
+    }
+}
---
+++ WebApiModels/ToDoViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace WebApiModels
+{
+    [JsonObject]
+    public class ToDoViewModel
+    {
+        [JsonProperty(PropertyName = "Id")]
+        public int Id { get; set; }
+
+        [JsonProperty(PropertyName = "Name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "IsComplete")]
+        [Display(Name = "Completed")]
+        public bool IsComplete { get; set; }
+
+        
+    }
+}
